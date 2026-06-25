f57ba8901f2a6ce0662e9201c673a7abc510a5a6
FreitasWendel <wendelfreitas627@gmail.com>
2026-06-25 20:38:02 -0300

    corrigindo erros

--- src/controllers/acervo.py
+++ src/controllers/acervo.py
@@ -1,18 +1,15 @@
-from flask import Blueprint, render_template, url_for, redirect
+from flask import Blueprint, render_template, url_for, redirect, request
+from src.models import db
 from src.models.livro import Livro
 
 acervo_bp = Blueprint('acervo', __name__, url_prefix='/acervo')
 
-@acervo_bp.route('/')
-def index():
-    return redirect(url_for('acervo.lista_livros'))
 
 @acervo_bp.route('/livros')
 def lista_livros():
     livros = Livro.query.all()
     return render_template('acervo/lista_livros.html', livros=livros)
-from flask import request
-from src.models import db
+
 @acervo_bp.route('/livros/novo', methods=['GET', 'POST'])
 def novo_livro():
 
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,8 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-from .usuario import Usuario
+from src.models.usuario import Usuario
 from src.models.livro import Livro
-from .emprestimo import Emprestimo
+from src.models.emprestimo import Emprestimo
 
 
